988a78d5185459a3342ba0aa2bca834c08f3a676
jiebaef <github.sfzfba@jiebaef.dev>
2023-12-24 15:24:07 +0100

    removed unnecessary vim options

--- init.lua
+++ init.lua
@@ -61,17 +61,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ MY SETTINGS ]]
-vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-
-vim.opt.wrap = false
-
-vim.opt.tabstop = 4
-
-vim.opt.clipboard = "unnamedplus"
 
 vim.opt.scrolloff = 999
 -- [[ END MY SETTINGS ]]
