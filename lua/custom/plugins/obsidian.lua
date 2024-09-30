5ce032ee1b670e69f5ff3c5b457349fdb8a5dfff
jiebaef <github.sfzfba@jiebaef.dev>
2024-09-30 10:13:39 +0200

    readded obsidian

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,43 +1,43 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
   lazy = true,
   -- ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   event = {
     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    "BufReadPre ~/Documents/vaults/FHDW/**.md",
-    "BufNewFile ~/Documents/vaults/FHDW/**.md",
+    'BufReadPre ~/Projects/Vaults/FHDW/**.md',
+    'BufNewFile ~/Projects/Vaults/FHDW/**.md',
   },
   dependencies = {
     -- Required.
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
 
     -- see below for full list of optional dependencies 👇
   },
   opts = {
     workspaces = {
       {
-        name = "fhdw",
-        path = "~/Documents/vaults/FHDW/",
+        name = 'fhdw',
+        path = '~/Projects/Vaults/FHDW/',
       },
     },
 
     daily_notes = {
       -- Optional, if you keep daily notes in a separate directory.
-      folder = "0_Daily notes",
+      folder = '0_Daily notes',
       -- Optional, if you want to change the date format for the ID of daily notes.
-      date_format = "%Y-%m-%d",
+      date_format = '%Y-%m-%d',
       -- Optional, if you want to change the date format of the default alias of daily notes.
       -- alias_format = "%B %-d, %Y",
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = "Daily template"
+      template = 'Daily template',
     },
     templates = {
-      subdir = "templates",
-      date_format = "%Y-%m-%d",
-      time_format = "%H:%M",
+      subdir = 'templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M',
       -- A map for custom variables, the key should be the variable and the value a function
       -- substitutions = {},
     },
