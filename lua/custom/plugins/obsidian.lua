74385e85147bffa926cdb3875d98578d9798936c
jiebaef <github.sfzfba@jiebaef.dev>
2024-10-08 10:41:24 +0200

    FEAT: changed obsidian to only load on laptop

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,46 +1,52 @@
-return {
-  'epwalsh/obsidian.nvim',
-  version = '*', -- recommended, use latest release instead of latest commit
-  lazy = false,
-  -- ft = 'markdown',
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    'BufReadPre " .. vim.fn.expand "~" .. "/Projects/Vaults/FHDW/**.md',
-    'BufNewFile " .. vim.fn.expand "~" .. "/Projects/Vaults/FHDW/**.md',
-  },
-  dependencies = {
-    -- Required.
-    'nvim-lua/plenary.nvim',
+local hostname = vim.loop.os_gethostname()
 
-    -- see below for full list of optional dependencies 👇
-  },
-  opts = {
-    workspaces = {
-      {
-        name = 'fhdw',
-        path = '~/Projects/Vaults/FHDW/',
+if hostname == 'framework-arch-finn' then
+  return {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = false,
+    -- ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      'BufReadPre " .. vim.fn.expand "~" .. "/Projects/Vaults/FHDW/**.md',
+      'BufNewFile " .. vim.fn.expand "~" .. "/Projects/Vaults/FHDW/**.md',
+    },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'fhdw',
+          path = '~/Projects/Vaults/FHDW/',
+        },
       },
-    },
 
-    daily_notes = {
-      -- Optional, if you keep daily notes in a separate directory.
-      folder = '0_Daily notes',
-      -- Optional, if you want to change the date format for the ID of daily notes.
-      date_format = '%Y-%m-%d',
-      -- Optional, if you want to change the date format of the default alias of daily notes.
-      -- alias_format = "%B %-d, %Y",
-      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = 'Daily template',
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = '0_Daily notes',
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = '%Y-%m-%d',
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        -- alias_format = "%B %-d, %Y",
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = 'Daily template',
+      },
+      templates = {
+        subdir = '9_Resources/templates',
+        date_format = '%Y-%m-%d',
+        time_format = '%H:%M',
+        -- A map for custom variables, the key should be the variable and the value a function
+        -- substitutions = {},
+      },
+      -- see below for full list of options 👇
     },
-    templates = {
-      subdir = '9_Resources/templates',
-      date_format = '%Y-%m-%d',
-      time_format = '%H:%M',
-      -- A map for custom variables, the key should be the variable and the value a function
-      -- substitutions = {},
-    },
-    -- see below for full list of options 👇
-  },
-}
+  }
+end
+
+return {}
