d5c78e088fe0fbc3b0a0dd4e548d6b347bd5053c
Finn <62570167+jiebaef@users.noreply.github.com>
2025-01-15 18:42:57 +0100

    Update lsp-servers.lua

--- lua/misc/lsp-servers.lua
+++ lua/misc/lsp-servers.lua
@@ -4,9 +4,24 @@ if require('misc.helpers').isFramework() then
   servers = {
     beautysh = {},
     csharp_ls = {},
+    cssls = {},
+    cssmodules_ls = {},
     djlint = {},
     docker_compose_language_service = {},
     dockerls = {},
+    emmet_language_server = {
+      filetypes = {
+        "css",
+        "html",
+        "javascriptreact",
+        "less",
+        "sass",
+        "scss",
+        "svelte",
+        "typescriptreact"
+      },
+    },
+    html = { filetypes = { 'html', 'twig', 'hbs' } },
     lua_ls = {
       settings = {
         Lua = {
@@ -17,6 +32,44 @@ if require('misc.helpers').isFramework() then
       },
     },
     rust_analyzer = {},
+    ts_ls = {
+      init_options = {
+        plugins = {
+          {
+            name = '@vue/typescript-plugin',
+            -- location = "P:/Scoop/apps/nvm/1.1.12/nodejs/nodejs/node_modules/@vue/typescript-plugin",
+            -- location = 'P:/Scoop/apps/nvm/current/nodejs/v22.1.0/node_modules/@vue/typescript-plugin',
+            location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+            languages = { 'javascript', 'typescript', 'vue' },
+          },
+        },
+      },
+      filetypes = {
+        'javascript',
+        'typescript',
+        'vue',
+      },
+    },
+    svelte = {
+      cmd = { 'svelteserver', '--stdio' },
+      settings = {
+        typescript = {
+          inlayHints = {
+            parameterNames = { enabled = 'all' },
+            parameterTypes = { enabled = false },
+            variableTypes = { enabled = true },
+            propertyDeclarationTypes = { enabled = true },
+            functionLikeReturnTypes = { enabled = false },
+            enumMemberValues = { enabled = true },
+          },
+        },
+      },
+      filetypes = { 'svelte' },
+      root_dir = {
+        'package.json',
+        '.git',
+      },
+    },
   }
 else
   servers = {
@@ -48,6 +101,8 @@ else
         },
       },
     },
+    powershell_es = {},
+
   }
 end
 
