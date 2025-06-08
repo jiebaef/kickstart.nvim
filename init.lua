64e0956d1b4ecfde6446d7559769db9faf649954
jiebaef <github.sfzfba@jiebaef.dev>
2025-06-08 13:36:59 +0200

    FIX: MSWindows can now move between nvim windows (ctrl + arrowkey)

--- init.lua
+++ init.lua
@@ -61,12 +61,17 @@ vim.keymap.set('n', '<leader><leader>x', '<cmd>source<Cr>', { desc = 'Sources th
 
 vim.g.netrw_list_hide = table.concat(helpers.ignored_netrw_list, ',')
 
--- print(helpers.isLinux())
-
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+if helpers.isLinux() then
+  vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+  vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+  vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+  vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+else
+  vim.keymap.set('n', '<C-Left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+  vim.keymap.set('n', '<C-Right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+  vim.keymap.set('n', '<C-Down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+  vim.keymap.set('n', '<C-Up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+end
 -- [[ END MY SETTINGS ]]
 
 -- The line beneath this is called `modeline`. See `:help modeline`
