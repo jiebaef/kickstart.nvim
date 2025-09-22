99052c949aba6e72926a23b0aec12e9d9da4e0d3
jiebaef <github.sfzfba@jiebaef.dev>
2025-09-22 23:45:28 +0200

    FEAT: add custom javascript tab settings

---
+++ after/ftplugin/javascript.lua
@@ -0,0 +1,7 @@
+local set = vim.opt_local
+
+-- set.shiftwidth = 2
+set.tabstop = 4 -- A TAB character looks like 4 spaces
+set.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+set.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+set.shiftwidth = 4 -- Number of spaces inserted when indenting
